a96fdebd0b7103a486ffc970305b629f777b7339
MrAlexHilton <alexander.hilton91@gmail.com>
2024-07-30 11:24:32 -0400

    hotel-reservation-app complete

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -32,10 +32,12 @@ export class ReservationFormComponent implements OnInit {
 
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     if(id){
-      let reservation = this.reservationService.getReservation(id);
-      if(reservation){
-        this.reservationForm.patchValue(reservation);
-      }
+      this.reservationService.getReservation(id)?.subscribe(reservation => {
+        if(reservation){
+          this.reservationForm.patchValue(reservation);
+        }
+      });
+      
     }
     
   }
@@ -46,9 +48,15 @@ export class ReservationFormComponent implements OnInit {
 
       let id = this.activatedRoute.snapshot.paramMap.get('id');
       if(id){
-          this.reservationService.updateReservation(id, reservation);
+        //Update
+          this.reservationService.updateReservation(id, reservation).subscribe(() =>{
+            console.log("Update request processed")
+          });
       } else {
-        this.reservationService.addReservation(reservation);
+        //New
+        this.reservationService.addReservation(reservation).subscribe(() =>{
+          console.log("Create request processed")
+        });
       }
 
       this.router.navigate(['/list'])
